57e11b09faa94cd29a5bb18857163fc3aa2db501
Dev-Rayo <159085113+Eliasivan@users.noreply.github.com>
2025-08-04 19:15:04 -0300

    Goku-Black-Bot-MD

--- plugins/ai-bard.js
+++ plugins/ai-bard.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 let handler = async (m, { text }) => {
   if (!text) {
-    return m.reply('${emoji} Ingresa un mensaje para enviar a la API.\n\nEjemplo: .bard Hola, ¿cómo estás?');
+    return m.reply('Ingresa un mensaje para enviar a la API.\n\nEjemplo: .bard Hola, ¿cómo estás?');
   }
 m.react('☄');
   const url = `https://api-rho-five-87.vercel.app/ai/bard?text=${encodeURIComponent(text)}`;
@@ -12,14 +12,14 @@ m.react('☄');
     const json = await res.json();
 
     if (!json.result || !json.result.response) {
-      return m.reply('#${emoji3} No se encontró la respuesta en el campo esperado.\n\n' + JSON.stringify(json));
+      return m.reply('No se encontró la respuesta en el campo esperado.\n\n' + JSON.stringify(json));
     }
 
     m.reply(json.result.response.trim());
 
   } catch (e) {
     console.error('ERROR al conectar con la API:', e);
-    m.reply('${emoji2} Error al conectar con la API:\n\n' + e.message);
+    m.reply('Error al conectar con la API:\n\n' + e.message);
   }
 };
 
